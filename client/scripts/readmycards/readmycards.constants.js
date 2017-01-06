a197bad6cf03c940d9a31e35f967d51bc3d2148e
Maarten Somers <maarten.somers@gmail.com>
2017-01-06 17:00:36 +0100

    Add event constants

---
+++ client/scripts/readmycards/readmycards.constants.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular.module('app.readmycards')
+        .constant('EVENTS', {
+            'GCL_INSTALLED': 'gcl-installed',
+            'CLOSE_SIDEBAR': 'close-sidebar',
+            'OPEN_SIDEBAR': 'open-sidebar',
+            'OPEN_FAQ': 'open-faq',
+            'START_OVER': 'again!',
+            'REINITIALIZE': 'reinit-viz',
+            'RETRY_READER': 'retry-reader',
+            'RETRY_CARD': 'retry-card'
+        })
+
+})();
