1a5e85ebaedd9a237dcc989a68ef7c5c2d4884f9
Maarten Somers <maarten.somers@gmail.com>
2017-01-25 14:39:50 +0100

    Add language variants for NMBS/SNCB and add Mobib basic card

--- client/scripts/cards/mobib/mobib.component.js
+++ client/scripts/cards/mobib/mobib.component.js
@@ -1,6 +1,13 @@
 (function () {
     'use strict';
 
+    const mobibBasic = {
+        templateUrl: 'views/cards/mobib/variants/mobib-basic.html',
+        binding: {
+            rnData: '>'
+        }
+    };
+
     const mobibDeLijn = {
         templateUrl: 'views/cards/mobib/variants/delijn.html',
         bindings: {
@@ -25,9 +32,27 @@
     const mobibNmbs = {
         templateUrl: 'views/cards/mobib/variants/nmbs.html',
         bindings: {
-            rnData: '<'
+            rnData: '<',
+            lang: '<'
         },
         controller: function () {
+            let controller = this;
+
+            controller.$onInit = () => {
+                if (controller.lang === 'nl') {
+                    controller.dobLabel = 'Geboortedatum :';
+                    controller.cardNumberLabel = 'Nº kaart :';
+                    controller.validityLabel = 'Geldigheid :';
+                    controller.logoUrl = 'images/nmbs-mobility.jpg';
+                }
+
+                if (controller.lang === 'fr') {
+                    controller.dobLabel = 'Date de naissance :';
+                    controller.cardNumberLabel = 'Nº carte :';
+                    controller.validityLabel = 'Validité :';
+                    controller.logoUrl = 'images/sncb-mobility.jpg'
+                }
+            }
 
         }
     };
@@ -53,6 +78,7 @@
     };
 
     angular.module('app.cards.mobib')
+        .component('mobibBasic', mobibBasic)
         .component('mobibDeLijn', mobibDeLijn)
         .component('mobibMivb', mobibMivb)
         .component('mobibNmbs', mobibNmbs)
