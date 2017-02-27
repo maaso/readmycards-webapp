546fff1fdd5ec70214f4586a0431b5a988dfb7a4
Maarten Somers <maarten.somers@gmail.com>
2017-02-27 11:59:21 +0100

    Fix undefined signData method

--- client/scripts/cards/lux/lux.service.js
+++ client/scripts/cards/lux/lux.service.js
@@ -87,7 +87,7 @@
 
         // OK
         function signWithGcl(readerId, pin, hash, algorithm) {
-            return T1C.luxeid.signData(readerId, pin, algorithm, hash).then(function (res) {
+            return T1C.luxId.signData(readerId, pin, algorithm, hash).then(function (res) {
                 return res.data;
             }, function (err) {
                 return $q.reject(err);
