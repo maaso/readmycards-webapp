568e51eecdb4bce4974ef3c943a7ff0747042e8b
Maarten Somers <maarten.somers@gmail.com>
2017-06-07 13:33:57 +0200

    T1C-519 Correctly handle cancelled request

--- client/scripts/cards/beid/beid.component.js
+++ client/scripts/cards/beid/beid.component.js
@@ -74,6 +74,10 @@
                             Analytics.trackEvent('beid', 'pin-blocked', 'Card blocked; too many incorrect attempts');
                             controller.pinStatus = 'blocked';
                             break;
+                        case 109:
+                            // cancelled on reader
+                            controller.pinStatus = 'cancelled';
+                            break;
                     }
                 });
             };
@@ -157,6 +161,7 @@
                 if (controller.status === '1remain') controller.infoText = 'Wrong PIN entered; 1 try remaining!';
                 if (controller.status === 'blocked') controller.infoText = '3 invalid PINs entered. Card blocked.';
                 if (controller.status === 'error') controller.infoText = 'An error occurred during the validation process. Please try again later.';
+                if (controller.status === 'cancelled') controller.infoText = 'Cancelled on reader, click to try again.';
             };
 
             controller.checkPin = () => {
