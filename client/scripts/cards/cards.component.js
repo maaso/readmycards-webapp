e6bd97d4cce5652c39730c7a7c8a0601fb5ff20a
Maarten Somers <maarten.somers@gmail.com>
2017-06-07 14:53:01 +0200

    T1C-519: Further generalise the pin check component

--- client/scripts/cards/cards.component.js
+++ client/scripts/cards/cards.component.js
@@ -13,13 +13,17 @@
         templateUrl: 'views/cards/pin-check-status.html',
         bindings: {
             status: '<',
-            maxTries: '<'
+            maxTries: '<',
+            okText: '<',
+            pinCheckFunc: '&'
         },
         controller: function (_) {
             let controller = this;
             controller.$onChanges = () => {
                 if (controller.status === 'idle') controller.infoText = 'Click to check PIN code';
-                if (controller.status === 'valid') controller.infoText = 'PIN check OK.';
+                if (controller.status === 'valid') controller.infoText = controller.okText;
+                if (controller.status === '4remain') controller.infoText = 'Wrong PIN entered; 4 tries remaining.';
+                if (controller.status === '3remain') controller.infoText = 'Wrong PIN entered; 3 tries remaining.';
                 if (controller.status === '2remain') controller.infoText = 'Wrong PIN entered; 2 tries remaining.';
                 if (controller.status === '1remain') controller.infoText = 'Wrong PIN entered; 1 try remaining!';
                 if (controller.status === 'blocked') controller.infoText = '' + controller.maxTries + ' invalid PINs entered. Card blocked.';
@@ -28,7 +32,7 @@
             };
 
             controller.checkPin = () => {
-                if (!_.includes(['valid', 'blocked'], controller.status)) controller.parent.checkPin();
+                if (!_.includes(['valid', 'blocked'], controller.status)) controller.pinCheckFunc()();
             }
         }
     };
