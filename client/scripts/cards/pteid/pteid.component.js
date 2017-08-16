39f3da2a586df0f983f6ad664d2ade6fe01dbae7
Maarten Somers <maarten.somers@gmail.com>
2017-08-16 18:19:32 +0200

    Upgrade to latest T1C-JS version

--- client/scripts/cards/pteid/pteid.component.js
+++ client/scripts/cards/pteid/pteid.component.js
@@ -153,7 +153,6 @@
             let controller = this;
 
             controller.$onInit = () => {
-                console.log(controller.idData);
                 let documentNumberComponents = _.split(controller.idData.document_number, " ");
                 controller.docNumberPart1 = _.pullAt(documentNumberComponents, 0)[0];
                 controller.docNumberPart2 = _.join(documentNumberComponents, " ");
