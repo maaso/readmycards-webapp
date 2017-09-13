d21855584991e74666be39378e7b3122533d0d23
Maarten Somers <maarten.somers@gmail.com>
2017-09-13 15:48:21 +0200

    Add basic EMV visualization

--- client/scripts/cards/emv/emv.component.js
+++ client/scripts/cards/emv/emv.component.js
@@ -1,14 +1,31 @@
 (function () {
     'use strict';
 
+    const emvCard = {
+        templateUrl: 'views/cards/emv/emv-card.html',
+        bindings: {
+            cardData: '<',
+        },
+        controller: function (_) {
+            let controller = this;
+
+            controller.$onInit = () => {
+                console.log(controller.cardData);
+            };
+        }
+    };
+
+    const emvVisualizer = {
+        templateUrl: 'views/cards/emv/emv-viz.html',
+        bindings: {
+            data: '<'
+        },
+        controller: function () {
+            let controller = this;
+        }
+    };
+
     angular.module('app.cards.emv')
-        .component('emvVisualizer', {
-            templateUrl: 'views/cards/emv/emv-viz.html',
-            bindings: {
-                panData: '<'
-            },
-            controller: function () {
-                let controller = this;
-            }
-        });
+           .component('emvCard', emvCard)
+           .component('emvVisualizer', emvVisualizer);
 })();
