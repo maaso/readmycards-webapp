b36c65ed345f57e4521f2b23ef55f8d3c02e7a2d
Maarten <maarten.somers@gmail.com>
2017-11-28 17:51:33 +0100

    Add support for new EMV api (WIP)

--- client/scripts/cards/emv/emv.component.js
+++ client/scripts/cards/emv/emv.component.js
@@ -1,14 +1,103 @@
 (function () {
     'use strict';
 
-    angular.module('app.cards.emv')
-        .component('emvVisualizer', {
-            templateUrl: 'views/cards/emv/emv-viz.html',
-            bindings: {
-                panData: '<'
-            },
-            controller: function () {
-                var controller = this;
+    const emvCard = {
+        templateUrl: 'views/cards/emv/emv-card.html',
+        bindings: {
+            cardData: '<',
+        },
+        controller: function (_, EmvUtils) {
+            let controller = this;
+
+            controller.$onInit = () => {
+                controller.cardNumber =
+                    EmvUtils.constructCardNumber(controller.cardData.application_data.pan);
+                controller.expiration =
+                    EmvUtils.constructExpirationDate(controller.cardData.application_data.expiration_date);
+            };
+        }
+    };
+
+    const emvPinCheckStatus = {
+        templateUrl: 'views/cards/pin-check-status.html',
+        bindings: {
+            status: '<'
+        },
+        require: {
+            parent: '^emvVisualizer'
+        },
+        controller: function (_) {
+            let controller = this;
+            controller.$onChanges = () => {
+                if (controller.status === 'idle') controller.infoText = 'Click to check PIN code';
+                if (controller.status === 'valid') controller.infoText = 'PIN check OK.';
+                if (controller.status === '2remain') controller.infoText = 'Wrong PIN entered; 2 tries remaining.';
+                if (controller.status === '1remain') controller.infoText = 'Wrong PIN entered; 1 try remaining!';
+                if (controller.status === 'blocked') controller.infoText = '3 invalid PINs entered. Card blocked.';
+                if (controller.status === 'error') controller.infoText = 'An error occurred during the validation process. Please try again later.';
+            };
+
+            controller.checkPin = () => {
+                if (!_.includes(['valid', 'blocked'], controller.status)) controller.parent.checkPin();
             }
-        });
+        }
+    };
+
+    const emvVisualizer = {
+        templateUrl: 'views/cards/emv/emv-viz.html',
+        bindings: {
+            data: '<',
+            readerId: '<'
+        },
+        controller: function ($uibModal, Connector) {
+            let controller = this;
+
+            controller.$onInit = () => {
+                controller.pinStatus = 'idle';
+            };
+
+            controller.checkPin = () => {
+                let modal = $uibModal.open({
+                    templateUrl: "views/readmycards/modals/check-pin.html",
+                    resolve: {
+                        readerId: () => {
+                            return controller.readerId
+                        },
+                        pinpad: () => {
+                            return Connector.get().core().reader(controller.readerId).then(res => {
+                                return res.data.pinpad;
+                            })
+                        },
+                        plugin: () => {
+
+                        }
+                    },
+                    backdrop: 'static',
+                    controller: 'ModalPinCheckCtrl'
+                });
+
+                modal.result.then(function () {
+                    controller.pinStatus = 'valid';
+                }, function (err) {
+                    switch (err.code) {
+                        case 103:
+                            controller.pinStatus = '2remain';
+                            break;
+                        case 104:
+                            controller.pinStatus = '1remain';
+                            break;
+                        case 105:
+                            Analytics.trackEvent('beid', 'pin-blocked', 'Card blocked; too many incorrect attempts');
+                            controller.pinStatus = 'blocked';
+                            break;
+                    }
+                });
+            };
+        }
+    };
+
+    angular.module('app.cards.emv')
+           .component('emvCard', emvCard)
+           .component('emvPinCheckStatus', emvPinCheckStatus)
+           .component('emvVisualizer', emvVisualizer);
 })();
---
+++ client/scripts/cards/emv/emv.service.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    angular.module('app.cards.emv')
+           .service('EmvUtils', EmvUtils);
+
+    function EmvUtils(_) {
+        this.constructCardNumber = constructCardNumber;
+        this.constructExpirationDate = constructExpirationDate;
+
+        function constructCardNumber(original) {
+            let cardNumber = "";
+            _.forEach(original, (comp, idx) => {
+                idx % 4 === 0 ? cardNumber += " " + comp : cardNumber += comp;
+            });
+            return cardNumber;
+        }
+
+        function constructExpirationDate(original) {
+            return moment(original, "YYMMDD").format("MM/YY");
+        }
+    }
+
+
+})();
